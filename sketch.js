33f74bbcfa9a6be201ccb7e7fa12b264ea0b47d2
Jassu0 <67613766+Jassu0@users.noreply.github.com>
2020-09-30 20:48:24 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,83 +1,120 @@
+var A,S,D,F;
+var ground;
+var groupA, groupS, groupD, groupF;
+var score = 0;
+var gamestate = "PLAY";
+var restart, restartimage;
 function preload(){ 
+    A = loadImage("A.png");
+    S = loadImage("S.png");
+    D = loadImage("D.png");
+    F = loadImage("F.png");
 }
 
 function setup(){
     createCanvas(1000,700);
-    
+    ground = createSprite(500,680,1000,40);
+    ground.shapeColor = "red";
+    groupA = new Group();
+    groupS = new Group();
+    groupD = new Group();
+    groupF = new Group();
+    textSize(18);
+    restart = createSprite(500,350);
+    restart.visible = false;
+    fill("white");
 }
 
 function draw(){
     background(0);
-    Tile1();
-    Tile2();
-    Tile3();
-    Tile4();
-    Tile5();
-    Tile6();
-    Tile7();
+    text("score: "+score, 80, 80);
+    if(gamestate == "PLAY") {
+        Tile1();
+        Tile2();
+        Tile3();
+        Tile4();
+        if(groupA.isTouching(ground) || groupS.isTouching(ground) || groupD.isTouching(ground) || groupF.isTouching(ground)) {
+            gamestate = "END";
+        }
+    }
+    else if(gamestate == "END") {
+        groupA.destroyEach();
+        groupS.destroyEach();
+        groupD.destroyEach();
+        groupF.destroyEach();
+        textSize(35);
+        fill("white");
+        stroke("white");
+        strokeWeight(2);
+        text("GAME OVER", 380, 300);
+        restart.visible = true;
+    }
     drawSprites();
 }   
 
 function Tile1() {
     if(frameCount%140 == 0) {
         var tile = createSprite(random(50,950),0,50,70);  
-        tile.shapeColor = "yellow";
+        tile.addImage(A);
         tile.velocityY = 5;
         tile.lifetime = 150;
+        tile.scale = 0.5;
+        groupA.add(tile);
     }
  
 }
 function Tile2() {
     if(frameCount%150 == 0) {
         var tile = createSprite(random(50,950),0,50,70);  
-        tile.shapeColor = "red";
+        tile.addImage(S);
         tile.velocityY = random(4,6);
         tile.lifetime = 150;
+        tile.scale = 0.5;
+        groupS.add(tile);
     }
  
 }
 function Tile3() {
     if(frameCount%160 == 0) {
         var tile = createSprite(random(50,950),0,50,70);  
-        tile.shapeColor = "lightblue";
+        tile.addImage(D);
         tile.velocityY = random(4,7);
         tile.lifetime = 150;
+        tile.scale = 0.5;
+        groupD.add(tile);
     }
  
 }
 function Tile4() {
     if(frameCount%170 == 0) {
         var tile = createSprite(random(50,950),0,50,70);  
-        tile.shapeColor = "green";
+        tile.addImage(F);
         tile.velocityY = random(4,7);
         tile.lifetime = 150;
+        tile.scale = 0.5;
+        groupF.add(tile);
     }
  
 }
-function Tile5() {
-    if(frameCount%180 == 0) {
-        var tile = createSprite(random(50,950),0,50,70);  
-        tile.shapeColor = "pink";
-        tile.velocityY = random(4,7);
-        tile.lifetime = 150;
+
+function keyPressed() {
+    if(gamestate == "PLAY") {
+        if(keyCode == 65 || keyCode == 97) {
+            groupA.destroyEach();
+            score += 5;
+        }
+        if(keyCode == 83 || keyCode == 115) {
+            groupS.destroyEach();
+            score += 5;
+        }
+        if(keyCode == 68 || keyCode == 100) {
+            groupD.destroyEach();
+            score += 5;
+        }
+        if(keyCode == 70 || keyCode == 102) {
+            groupF.destroyEach();
+            score += 5;
+        }
     }
- 
-}
-function Tile6() {
-    if(frameCount%175 == 0) {
-        var tile = createSprite(random(50,950),0,50,70);  
-        tile.shapeColor = "orange";
-        tile.velocityY = random(4,7);
-        tile.lifetime = 150;
-    }
- 
-}
-function Tile7() {
-    if(frameCount%185 == 0) {
-        var tile = createSprite(random(50,950),0,50,70);  
-        tile.shapeColor = "purple";
-        tile.velocityY = random(4,7);
-        tile.lifetime = 150;
-    }
- 
+
 }
