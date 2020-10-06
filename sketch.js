f792b1537f979717c9163a6985536a08015853d9
Jassu0 <67613766+Jassu0@users.noreply.github.com>
2020-10-05 20:47:45 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,33 +1,44 @@
-var A,S,D,F;
+var A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
 var ground;
-var groupA, groupS, groupD, groupF;
+var groupA,groupB,groupC,groupD,groupE,groupF,groupG,groupH,groupI,groupJ,groupK,groupL,groupM,groupN,
+groupO,groupP,groupQ,groupR,groupS,groupT,groupU,groupV,groupW,groupX,groupY,groupZ;
 var score = 0;
 var gamestate = "PLAY";
 var restart, restartimage;
+var statusA,statusB,statusC,statusD,statusE,statusF,statusG,statusH,statusI,statusJ,statusK,statusL,statusM,statusN,
+statusO,statusP,statusQ,statusR,statusS,statusT,statusU,statusV,statusW,statusX,statusY,statusZ;
+var bgimage;
+var click;
 function preload(){ 
     A = loadImage("A.png");
     S = loadImage("S.png");
     D = loadImage("D.png");
     F = loadImage("F.png");
+    Reset = loadImage("Restart.png");
+    bgimage = loadImage("Bg.jpg");
+    click = loadSound("Click.mp3");
 }
 
 function setup(){
-    createCanvas(1000,700);
-    ground = createSprite(500,680,1000,40);
-    ground.shapeColor = "red";
+    createCanvas(displayWidth-20,displayHeight-50);
+    ground = createSprite(width/2,height,width,150);
+    ground.visible = false;
+    //ground.shapeColor = "black";
     groupA = new Group();
     groupS = new Group();
     groupD = new Group();
     groupF = new Group();
     textSize(18);
-    restart = createSprite(500,350);
+    restart = createSprite(width/2,height/2+80);
+    restart.addImage(Reset);
+    restart.scale = 0.5;
     restart.visible = false;
     fill("white");
 }
 
 function draw(){
-    background(0);
-    text("score: "+score, 80, 80);
+    background(bgimage);
+    text("score: "+score, width/7, height/7);
     if(gamestate == "PLAY") {
         Tile1();
         Tile2();
@@ -46,75 +57,103 @@ function draw(){
         fill("white");
         stroke("white");
         strokeWeight(2);
-        text("GAME OVER", 380, 300);
+        text("GAME OVER", width/2-100, height/2);
         restart.visible = true;
+        if(mousePressedOver(restart)) {
+            reset();
+        }
     }
     drawSprites();
 }   
-
+function reset() {
+    restart.visible = false;
+    gamestate = "PLAY";
+    score = 0;
+}
 function Tile1() {
     if(frameCount%140 == 0) {
-        var tile = createSprite(random(50,950),0,50,70);  
+        var tile = createSprite(random(50,width-50),0,50,70);  
         tile.addImage(A);
         tile.velocityY = 5;
         tile.lifetime = 150;
         tile.scale = 0.5;
         groupA.add(tile);
+        statusA = 0;
     }
  
 }
 function Tile2() {
     if(frameCount%150 == 0) {
-        var tile = createSprite(random(50,950),0,50,70);  
+        var tile = createSprite(random(50,width-50),0,50,70);  
         tile.addImage(S);
         tile.velocityY = random(4,6);
         tile.lifetime = 150;
         tile.scale = 0.5;
         groupS.add(tile);
+        statusS = 0;
     }
  
 }
 function Tile3() {
     if(frameCount%160 == 0) {
-        var tile = createSprite(random(50,950),0,50,70);  
+        var tile = createSprite(random(50,width-50),0,50,70);  
         tile.addImage(D);
         tile.velocityY = random(4,7);
         tile.lifetime = 150;
         tile.scale = 0.5;
         groupD.add(tile);
+        statusD = 0;
     }
  
 }
 function Tile4() {
     if(frameCount%170 == 0) {
-        var tile = createSprite(random(50,950),0,50,70);  
+        var tile = createSprite(random(50,width-50),0,50,70);  
         tile.addImage(F);
         tile.velocityY = random(4,7);
         tile.lifetime = 150;
         tile.scale = 0.5;
         groupF.add(tile);
+        statusF = 0;
     }
  
 }
 
 function keyPressed() {
     if(gamestate == "PLAY") {
-        if(keyCode == 65 || keyCode == 97) {
-            groupA.destroyEach();
-            score += 5;
+        if(statusA == 0) {
+            if(keyCode == 65 || keyCode == 97) {
+                groupA.destroyEach();
+                score += 5;
+                statusA = 1;
+                click.play();
+            }
         }
-        if(keyCode == 83 || keyCode == 115) {
-            groupS.destroyEach();
-            score += 5;
+        if(statusS == 0) {
+          if(keyCode == 83 || keyCode == 115) {
+                groupS.destroyEach();
+                score += 5;
+                statusS = 1;
+                click.play();
+            }
         }
-        if(keyCode == 68 || keyCode == 100) {
-            groupD.destroyEach();
-            score += 5;
+        if(statusD == 0) {
+           if(keyCode == 68 || keyCode == 100) {
+                groupD.destroyEach();
+                score += 5;
+                statusD = 1;
+                click.play();
+            } 
         }
-        if(keyCode == 70 || keyCode == 102) {
-            groupF.destroyEach();
-            score += 5;
+        if(statusF == 0) {
+          if(keyCode == 70 || keyCode == 102) {
+                groupF.destroyEach();
+                score += 5;
+                statusF = 1;
+                click.play();
+            }  
         }
+        
     }
 
 }
